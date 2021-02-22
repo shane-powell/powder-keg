09dcf22b2bdd3a19beb642a896183f8930ed9175
Shane Powell <kenjara@hotmail.com>
2021-02-22 12:01:31 +0000

    Removed debug folder.

--- cmake-build-debug/32blit/version_defs.hpp
+++
@@ -1,4 +0,0 @@
-#pragma once
-
-#define	BLIT_BUILD_DATE "2020-08-27"
-#define BLIT_BUILD_VER "DEV"
--- cmake-build-debug/assets.hpp
+++
@@ -1,5 +0,0 @@
-// Auto Generated File - DO NOT EDIT!
-#pragma once
-#include <cstdint>
-extern const uint8_t sprites_data[];
-extern const uint32_t sprites_data_length;
